aba45e98891276be115bf7fc5a635b36783f4049
Rabyray420 <rabyray420@gmail.com>
2025-02-11 14:02:00 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,18 @@
+//Константы для валидации:
 const form = document.forms["feedback-form"];
 const formArr = Array.from(form);
 const validFormArr = [];
 const button = form.elements["feedback-form-button"];
+//Константы для счётчика:
+const textArea = document.querySelector(".feedback-form__textarea");
+const counter = document.querySelector(".counter-text__current");
+
+//Счётчик символов:
+textArea.addEventListener("input", onInput);
+function onInput(evt) {
+  const length = evt.target.value.length;
+  counter.innerText = length;
+}
 
 formArr.forEach((el) => {
   if (el.hasAttribute("data-reg")) {
@@ -10,6 +21,7 @@ formArr.forEach((el) => {
   }
 });
 
+//Дальше валидация:
 form.addEventListener("input", inputHandler);
 button.addEventListener("click", buttonHandler);
 
@@ -23,6 +35,7 @@ function inputCheck(el) {
   const inputValue = el.value;
   const inputReg = el.getAttribute("data-reg");
   const reg = new RegExp(inputReg);
+
   if (reg.test(inputValue)) {
     el.setAttribute("is-valid", "1");
     el.style.border = "2px solid rgb(0, 196, 0)";
@@ -37,14 +50,51 @@ function buttonHandler(e) {
   validFormArr.forEach((el) => {
     allValid.push(el.getAttribute("is-valid"));
   });
+
   const isAllValid = allValid.reduce((acc, current) => {
-    return acc && current;
+    return acc & current;
+    // Здесь было "&&", но оно не работало как надо.
+    // Почему "&" работает, я не знаю
+    // Наверное что-то с типами данных, строки, хуёки
+    // 3 дня проебался, а надо было просто почитать комментарии
+
+    // ⠄⠄⠄⠄⠄⠄⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡀⠄⠄⠄⠄⠄⠄
+    // ⠄⠄⠄⠄⠄⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡀⠄⠄⠄⠄⠄
+    // ⠄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠄⠄⠄⠄
+    // ⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⡿⠛⠃⢈⣁⡀⠈⠄⠄⠄⣙⡉⠙⠿⣿⡄⠄⠄⠄
+    // ⠄⠄⠄⠄⢻⣿⣿⣿⣿⠟⠉⠄⠈⠠⠄⠄⠄⠄⠄⠄⢠⠄⢀⡁⠄⣿⠃⠄⠄⠄
+    // ⠄⠄⠄⠄⢸⣿⣿⣿⠃⠄⠄⠠⠂⢖⣶⣶⡢⠄⠄⠄⠄⣤⣤⣤⠁⡎⠄⠄⡀⠄
+    // ⠄⠄⠄⠄⠘⣿⣿⠃⠄⢀⠄⠄⠄⠐⠒⠾⠋⠄⠄⠄⠄⠙⠿⠣⠄⢱⠄⠄⠄⠄
+    // ⠄⠄⠄⠄⠄⠈⠻⠄⠄⢸⠄⠄⠄⠄⠄⠄⢀⡀⠄⠄⠈⢢⠄⠄⢀⢸⠄⠄⠁⠄
+    // ⠄⠄⠄⠄⠄⠄⠄⡇⠄⠄⢆⠄⠁⠄⠄⠠⠘⠤⠐⠄⠠⢆⠄⠄⢠⠃⢠⠄⠄⠄
+    // ⠄⠄⠄⠄⠄⠄⠄⡇⠄⠄⡎⠳⣄⠄⠄⠁⠤⠤⠄⠄⢀⣄⠄⢰⠁⠄⠄⠄⠄⠄
+    // ⠄⠄⣠⣴⣶⣿⣿⠁⠄⠄⠁⠄⠈⠑⢄⠄⠄⠄⠄⠄⠄⠈⡠⠋⢀⠄⠄⠄⠄⠄
+
+    //     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⡶⠛⠛⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⠋⠀⠀⠀⠈⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⢀⣠⠴⠞⠛⠉⠉⠉⠉⠉⠉⠛⠒⠾⢤⣀⠀⣀⣠⣤⣄⡀⠀⠀⠀
+    // ⠀⠀⠀⣠⡶⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢭⡀⠀⠈⣷⠀⠀⠀
+    // ⠀⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢦⢀⡟⠀⠀⠀
+    // ⠀⣾⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⡅⠀⠀⠀
+    // ⢸⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣄⣀⠀
+    // ⣾⠀⠀⣠⣤⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣤⣄⠀⠀⠀⠀⠀⠀⠸⡇⠉⣷
+    // ⣿⠀⠰⣿⣿⣿⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⣧⡴⠋
+    // ⣿⠀⠀⢸⠛⢫⠀⠀⢠⠴⠒⠲⡄⠀⠀⠀⠀⡝⠛⢡⠀⠀⠀⠀⠀⠀⢰⡏⠀⠀
+    // ⢸⡄⠀⢸⡀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⢸⠀⠀⠀⠀⠀⠀⡼⣄⠀⠀
+    // ⠀⢳⡄⠀⡇⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⠀⢸⠀⠀⠀⠀⢀⡼⠁⢸⡇⠀
+    // ⠀⠀⠙⢦⣷⡈⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠈⡇⠀⣀⡴⠟⠒⠚⠋⠀⠀
+    // ⠀⠀⠀⠀⠈⠛⠾⢤⣤⣀⣀⡀⠀⠀⠀⠀⣀⣈⣇⡤⣷⠚⠉⠀⠀⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⠀⠀⣰⠇⠀⠩⣉⠉⠉⠉⣩⠍⠁⠀⢷⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⠀⠀⡟⠐⠦⠤⠼⠂⠀⠸⠥⠤⠔⠂⠘⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⠀⣸⣧⡟⠳⠒⡄⠀⠀⠀⡔⠲⠚⣧⣀⣿⠿⠷⣶⡆⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⠀⠻⣄⢀⠀⠀⡗⠀⠀⠀⡇⠄⢠⠀⣼⠟⠀⢀⣨⠇⠀⠀⠀⠀⠀
+    // ⠀⠀⠀⠀⠀⠀⠀⠙⢶⠬⠴⢧⣤⣤⣤⣽⣬⡥⠞⠛⠛⠋⠉⠀⠀
   });
+
+  console.log(validFormArr);
 
   if (!Boolean(Number(isAllValid))) {
     e.preventDefault();
     alert("Форма не валидна!");
-  } else {
-    alert("Форма ОТПРАВНЕНА!");
   }
 }
